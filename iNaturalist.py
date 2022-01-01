9b2378210381e2625c39e7b6384edc4c9c35dfb7
Manuel Popp <34488159+ManuelPopp@users.noreply.github.com>
2022-01-01 15:21:04 +0100

    Update iNaturalist.py

--- iNaturalist.py
+++ iNaturalist.py
@@ -28,9 +28,8 @@ observations = []
 page_results = [1]
 p = 1
 while len(page_results) > 0:
-    page_results = get_observations(project_id = project_ID, \
-                                        per_page = 30, page = p)
-    page_results = page_results["results"]
+    page = get_observations(project_id = project_ID, per_page = 30, page = p)
+    page_results = page["results"]
     observations += page_results
     p += 1
 
